db7cad243151acdc4d88f3ed11df3572407d9d59
TripUpCumilla <farokm910@gmail.com>
2026-03-22 15:29:28 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyC_qgj5IEeK7n3soI0o6EKuOwdyCpTA14s",
+  authDomain: "trip-d13c6.firebaseapp.com",
+  projectId: "trip-d13c6",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
