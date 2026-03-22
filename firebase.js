80e68dbb49ae3412ccc726f1a87a703680893ea2
TripUpCumilla <farokm910@gmail.com>
2026-03-22 16:14:18 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -12,3 +12,26 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC_qgj5IEeK7n3soI0o6EKuOwdyCpTA14s",
+  authDomain: "trip-d13c6.firebaseapp.com",
+  projectId: "trip-d13c6",
+  storageBucket: "trip-d13c6.firebasestorage.app",
+  messagingSenderId: "245889992500",
+  appId: "1:245889992500:web:931fb5ad7976cfa9898761",
+  measurementId: "G-S3MQ2RFTV9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
